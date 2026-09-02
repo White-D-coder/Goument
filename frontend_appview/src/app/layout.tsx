949563932e5f51604e1380388ff2604790f62efd
White-D-coder <futuristicthought07@gmail.com>
2026-09-02 21:09:46 +0530

    clarity

--- frontend_appview/src/app/layout.tsx
+++ frontend_appview/src/app/layout.tsx
@@ -81,6 +81,15 @@ export default async function RootLayout({
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "yc2tnq4gqa");
+          `}
+        </Script>
         <QueryProvider>
           <ResponsiveShell>{children}</ResponsiveShell>
         </QueryProvider>
